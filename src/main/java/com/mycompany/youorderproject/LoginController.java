51e56b2c4cb6e37f46f61d64c06999aa5325b440
Carlos Henrique <carlos_henrique97@outlook.com.br>
2022-05-05 19:15:03 -0300

    Criação de alguns eventos de botões e layout de fundo

--- src/main/java/com/mycompany/youorderproject/LoginController.java
+++ src/main/java/com/mycompany/youorderproject/LoginController.java
@@ -9,8 +9,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 /**
  * FXML Controller class
  *
@@ -21,6 +26,18 @@ public class LoginController implements Initializable {
 
     @FXML
     private Button novoCadastroButton;
+    
+    @FXML
+    private Button btnAcessar;
+    
+    @FXML
+    private Label lblEsqueceuASenha;
+    
+    @FXML
+    private TextField txtLogin;
+    
+    @FXML
+    private PasswordField txtSenha;
     /**
      * Initializes the controller class.
      */
@@ -32,6 +49,24 @@ public class LoginController implements Initializable {
     @FXML
     private void switchToNovoCadastro() throws IOException {
         App.setRoot("novoCadastro");
+    }
+    
+    @FXML
+    private void lblEsqueceuASenhaOnMouseClicked() throws IOException {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Aviso");
+        alert.setHeaderText("Esqueceu a senha?");
+        alert.setContentText("Poxa, essa opção ainda está em desenvolvimento. Aguarde as próximas atualizações.");
+        alert.showAndWait();
+    }
+    
+    @FXML
+    private void btnAcessarOnMouseClicked() throws IOException {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Aviso");
+        alert.setHeaderText("Acesso ao Login");
+        alert.setContentText("Poxa, essa opção ainda está em desenvolvimento. Aguarde as próximas atualizações.");
+        alert.showAndWait();
     }
 
 }
--- src/main/java/com/mycompany/youorderproject/NovoCadastroController.java
+++ src/main/java/com/mycompany/youorderproject/NovoCadastroController.java
@@ -4,9 +4,14 @@
  */
 package com.mycompany.youorderproject;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -15,6 +20,11 @@ import javafx.fxml.Initializable;
  */
 public class NovoCadastroController implements Initializable {
 
+    @FXML
+    private Button btnRegistrar;
+    @FXML
+    private Button btnVoltar;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +32,19 @@ public class NovoCadastroController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnRegistrarOnMouseClicked(MouseEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Aviso");
+        alert.setHeaderText("Registrar novo usuário");
+        alert.setContentText("Poxa, essa opção ainda está em desenvolvimento. Aguarde as próximas atualizações.");
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void btnVoltarOnMouseClicked(MouseEvent event) throws IOException {
+        App.setRoot("login");
+    }
     
 }
