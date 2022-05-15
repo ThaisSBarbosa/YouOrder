ca9a9aab9b2246f18d9971772a23f7dbca2aa925
ThaisSBarbosa <thssantosbarbosa@gmail.com>
2022-05-15 13:38:02 -0300

    Projeto refatorado rodando.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.youorderproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.youorderproject to javafx.fxml;
-    exports com.mycompany.youorderproject;
-}
---
+++ src/test/java/br/edu/cefsa/youorderproject_test/PrincipalTest.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.cefsa.youorderproject_test;
+
+/**
+ *
+ * @author thais
+ */
+public class PrincipalTest {
+    
+}
